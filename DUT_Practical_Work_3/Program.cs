6099afabc19e16c5baa6316dc61b83eb15c3197b
V.Khaletskiy <vitallyhalez.gmail.com>
2019-09-11 10:33:56 +0300

    Third practical work

---
+++ DUT_Practical_Work_3/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DUT_Practical_Work_3 {
+	class Program {
+		static void Main(string[] args) {
+			CalculateHelper helper = new CalculateHelper();
+			helper.Calculate();
+		}
+	}
+
+	class CalculateHelper {
+		private OutputProcessingHelper _helper;
+
+		private OutputProcessingHelper helper {
+			get {
+				return _helper ?? (
+					_helper = new OutputProcessingHelper()
+				);
+			}
+		}
+
+		public void Calculate() {
+			double x = helper.GetConsoleValue<double>("\nEnter x:");
+			double y = helper.GetConsoleValue<double>("Enter y:");
+
+			if (ValidateParameters(x, y)) {
+				Console.WriteLine("Parameters in second part");
+			}
+
+			Calculate();
+		}
+
+		private bool ValidateParameters(double x, double y) {
+			bool isValidate = x < 0 && y > 0;
+
+			if (!isValidate) {
+				Console.WriteLine("Parameters isn`t in second part");
+			}
+
+			return isValidate;
+		}
+	}
+
+	class OutputProcessingHelper {
+
+		public T GetConsoleValue<T>(string description) {
+			Console.WriteLine(description);
+			return SafeGetTypedValue<T>(Console.ReadLine());
+		}
+
+		public T SafeGetTypedValue<T>(string value) {
+			try {
+				TypeConverter converter = TypeDescriptor.GetConverter(typeof(T));
+				if (converter != null) {
+					return (T)converter.ConvertFromString(value);
+				}
+				return default(T);
+			} catch (NotSupportedException) {
+				return default(T);
+			}
+		}
+	}
+}
